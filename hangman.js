0494be627eeed8ebd0c15ff683dbe7e2775d9c1f
Zachary N. Dunn <zacharyn.dunn@Edmo.local>
2018-06-25 20:47:46 -0700

    -

---
+++ hangman.js
@@ -0,0 +1,14 @@
+function myFunction1() {
+    document.getElementById("head1").style.visibility = "visible";
+}
+function myFunction2() {
+    document.getElementById("chest").style.visibility = "visible";
+}
+function myFunction3() {
+    document.getElementById("legs").style.visibility = "visible";
+}
+function myFunction4() {
+    document.getElementById("head1").style.visibility = "hidden";
+    document.getElementById("chest").style.visibility = "hidden";
+    document.getElementById("legs").style.visibility = "hidden";
+}
